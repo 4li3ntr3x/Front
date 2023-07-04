d59755565786c9e4ee5dd6a46945cb69284a8d74
4li3ntr3x <4li3ntr3x@gmail.com>
2023-07-04 23:18:27 +0200

    test

---
+++ js/productos.js
@@ -0,0 +1,46 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        url:"http://4li3ntr3x.pythonanywhere.com/productos",
+        productos:[],
+        error:false,
+        cargando:true
+      }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        eliminar(producto) {
+            const url = 'http://4li3ntr3x.pythonanywhere.com/productos/' + producto;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+                    location.reload();
+                })
+        }
+
+
+    },
+    
+
+
+
+  }).mount('#app')
